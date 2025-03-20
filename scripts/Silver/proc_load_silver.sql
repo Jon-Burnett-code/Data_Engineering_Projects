c301f827932e22bbf663e171b87e34ab8e8c6e6e
Jon Burnett <jonaburnett@yahoo.co.uk>
2025-03-20 10:28:19 +0000

    Update proc_load_silver.sql

--- scripts/Silver/proc_load_silver.sql
+++ scripts/Silver/proc_load_silver.sql
@@ -85,5 +85,15 @@ END AS sls_ship_date,
 CASE WHEN sls_due_date = 0 OR LEN(sls_due_date) !=8 THEN NULL
 	ELSE CAST(CAST(sls_due_date AS VARCHAR) AS DATE)
 END AS sls_due_date
-
+CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+	 THEN sls_quantity * ABS(sls_price)
+	 ELSE sls_sales
+END AS sls_sales,
+sls_quantity,
+CASE WHEN sls_price IS NULL OR sls_price <= 0 
+	 THEN sls_sales / NULLIF(sls_quantity, 0) 
+	 ELSE sls_price
+END AS sls_price
 FROM bronze.crm_sales_details;
+
+
