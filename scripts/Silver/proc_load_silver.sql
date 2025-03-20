2f5ed64690a7ef8c13545244fb1c76bb6e72e340
Jon Burnett <jonaburnett@yahoo.co.uk>
2025-03-20 12:04:25 +0000

    Update proc_load_silver.sql

--- scripts/Silver/proc_load_silver.sql
+++ scripts/Silver/proc_load_silver.sql
@@ -96,4 +96,17 @@ CASE WHEN sls_price IS NULL OR sls_price <= 0
 END AS sls_price
 FROM bronze.crm_sales_details;
 
+TRUNCATE TABLE silver.erp_loc_a101;
+INSERT INTO silver.erp_loc_a101 (
+	cid,
+	cntry
+)
+SELECT 
+REPLACE(cid, '-', '') cid,
+CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+	 WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+	 WHEN Trim(cntry)  = '' OR cntry IS NULL THEN 'n/a'
+	 ELSE TRIM(cntry)
+END AS cntry
+FROM bronze.erp_loc_a101;
 
