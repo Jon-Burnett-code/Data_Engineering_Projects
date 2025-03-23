1022b786b32c0411f06f63787ae5e3b4be36db51
Jon Burnett <jonaburnett@yahoo.co.uk>
2025-03-23 19:02:31 +0000

    Create checks_for_bronze.sql

---
+++ SQL Data Warehouse Project/Scripts/Silver/checks_for_bronze.sql
@@ -0,0 +1,89 @@
+/* 
+
+
+*/ 
+--==================================================
+-->>Performing Checks for all of the bronze data
+--==================================================
+
+--==================================================
+-->> Performing Checks for bronze.crm_cust_info
+--==================================================
+
+-- Check For Nulls or Dublicates in Primary Key
+-- Expectation: No results
+SELECT 
+cst_id,
+COUNT(*)
+FROM bronze.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+--Check for unwanted spaces
+--Expectation: No Results
+SELECT cst_firstname
+FROM bronze.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname);
+
+SELECT cst_lastname
+FROM bronze.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname);
+
+-- Data Standardization & Consistency
+SELECT DISTINCT cst_gndr
+FROM bronze.crm_cust_info;
+
+SELECT DISTINCT cst_material_status
+FROM bronze.crm_cust_info;
+
+--==================================================
+-->> Perfroming Checks for bronze.crm_prd_info
+--==================================================
+
+--Check for Nulls or Duplicates in Primary Key
+--Expectations: No Results 
+SELECT 
+prd_id,
+COUNT(*)
+FROM bronze.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+--Checking for Unwanted Spaces
+--Expected Result: No results
+SELECT prd_nm
+FROM bronze.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+--Checking for Nulls or Negative Numbers
+--Expectations: No results
+SELECT prd_cost
+FROM bronze.crm_prd_info
+WHERE prd_cost IS null OR prd_cost < 0;
+
+--Data Standardization & Consistency
+SELECT DISTINCT prd_line
+FROM bronze.crm_prd_info;
+
+--Check for Invalid Date Orders
+SELECT *
+FROM bronze.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+
+--==================================================
+-->> Performing Checks for bronze.crm_sales_details
+--==================================================
+
+--Checking for Unwanted Spaces
+--Expectation: No results
+SELECT * FROM bronze.crm_sales_details
+WHERE sls_ord_num != TRIM(sls_ord_num);
+
+--Checking everything working between sls_prd_key >> pr_key
+--Expectation: No results
+SELECT * FROM bronze.crm_sales_details
+WHERE sls_prd_key NOT IN (SELECT prd_key FROM silver.crm_prd_info);
+--Checking everything working between sls_cust_id >> cust_id
+--Expectation: No results
+SELECT * FROM bronze.crm_sales_details
+WHERE sls_cust_id NOT IN (SELECT cst_id FROM silver.crm_cust_info);
